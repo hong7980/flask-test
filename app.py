ab1386da07081b62801bc7f835c472c32c6c33f5
hong7980 <873249275@qq.com>
2020-02-13 15:58:07 +0800

    pycharm提交v0.2

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def hello2(name):
 @app.route('/test')
 def test_url_for():
     print(url_for('hello'))
-    print(url_for('hello2',name= hong))
+    print(url_for('hello2',name= 'hong'))
+    print(url_for('hello2',name='hongshui'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for',num=2))
+    return  'Test page'
 
