dfaafb3733ec12c94ceab897ee5920c75a6be3fb
hong7980 <873249275@qq.com>
2020-02-13 20:36:51 +0800

    pycharm提交v0.2

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 app =Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello'
+#@app.route('/')
+#def hello():
+ #   return 'Hello'
 
 @app.route('/user/<name>')
 def hello2(name):
@@ -18,3 +18,13 @@ def test_url_for():
     print(url_for('test_url_for',num=2))
     return  'Test page'
 
+@app.route('/')
+def index():
+    return render_template('index.html',name=name,movies=movies)
+
+name ='hong'
+movies = [{'title':'book1','year':'1988'},
+        {'title':'book2','year':'1488'},
+        {'title':'book3','year':'1978'},
+{'title':'book3','year':'1978'},
+]
