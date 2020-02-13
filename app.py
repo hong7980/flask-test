796b00192a561abc52df1c1a289d86ee8a93228f
hong7980 <873249275@qq.com>
2020-02-13 15:54:10 +0800

    pycharm提交v0.1

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask,url_for
 app =Flask(__name__)
 
 @app.route('/')
 def hello():
-          return'Welcome'
+    return 'Hello'
+
+@app.route('/user/<name>')
+def hello2(name):
+          return'Welcome,%s' % name
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('hello2',name= hong))
 
